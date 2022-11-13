795bdc30aaf733239f2917d207cc3185912e77b6
Juanppmi <116412973+Juanppmi@users.noreply.github.com>
2022-11-12 21:59:52 -0300

    Programa sem mudança

---
+++ TesteEnvio.py
@@ -0,0 +1,12 @@
+escolha = input("Escolha: Juan ou Gustavo\n")
+
+match escolha:
+    case "Juan":
+        print("Juan:\n")
+        print("Olha a olha onda olha a onda pan pan")
+
+    case "Gustavo":
+        print("Gustavo:\n")
+
+    case _:
+        print("Opa chefia, se pa que tu errou o nome, letra maiscula importa.")
